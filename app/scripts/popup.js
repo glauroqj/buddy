66bda948e1125135e07a38f1c91d5a48e101207d
glauroqj <glauro.juliani@hotmail.com>
2017-09-15 00:08:04 -0300

    validação após votar, liberado votar novamente 24hrs depois

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,7 +1,7 @@
 options = {
   namespace: 'vuejs__'
 };
- 
+
 Vue.use(VueLocalStorage, options);
 
 new Vue({
@@ -12,6 +12,7 @@ new Vue({
     title: 'Olá, fala pra gente? Como está seu dia?',
     firstLogin: true,
     vote: false,
+    voted: '',
     company: 'Sympla',
     registration: '',
     name: '',
@@ -19,12 +20,21 @@ new Vue({
   },
   mounted() {
     /* verify first login on buddy */
-    this.user = Vue.ls.get('Buddy');
+    this.user = Vue.ls.get('Buddy-Login');
     console.log(this.user)
     if ( this.user === '' || this.user === null ) {
       this.firstLogin = true;
     } else {
       this.firstLogin = false;
+    }
+
+    /* verify last vote on buddy */
+    this.voted = Vue.ls.get('Buddy-Vote');
+    console.log(this.voted)
+    if ( this.voted === '' || this.voted === null ) {
+      this.vote = false;
+    } else {
+      this.vote = true;
     }
   },
   methods: {
@@ -39,12 +49,13 @@ new Vue({
         name: this.name,
         sector: this.sector
       }
-      Vue.ls.set('Buddy', info, 60 * 60 * 100000);
+      Vue.ls.set('Buddy-Login', info, 60 * 60 * 100000);
       this.firstLogin = false;
     },
     send: function(value) {
       console.log(value)
       this.vote = true;
+      Vue.ls.set('Buddy-Vote', value, 60 * 60 * 24 * 1000); /* 24 hours */
     }
   }
 });
