0741db2cdfca13c9060a9cd032f9d487ad3762e9
glauroqj <glauro.juliani@hotmail.com>
2017-09-14 23:54:51 -0300

    inclusão novos templates, validação primeiro login com localstorage

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,4 +1,3 @@
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     console.log('request ', request.text);
-
 });
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,14 +1,50 @@
+options = {
+  namespace: 'vuejs__'
+};
+ 
+Vue.use(VueLocalStorage, options);
+
 new Vue({
   name: 'Buddy',
   el: '#buddy',
   data: {
-    url: undefined,
+    user: '',
     title: 'Olá, fala pra gente? Como está seu dia?',
+    firstLogin: true,
+    vote: false,
+    company: 'Sympla',
+    registration: '',
+    name: '',
+    sector: ''
   },
-  mounted: function() {
-
+  mounted() {
+    /* verify first login on buddy */
+    this.user = Vue.ls.get('Buddy');
+    console.log(this.user)
+    if ( this.user === '' || this.user === null ) {
+      this.firstLogin = true;
+    } else {
+      this.firstLogin = false;
+    }
   },
   methods: {
-
+    reset: function() {
+      this.vote = false;
+      this.firstLogin = true;
+    },
+    login: function() {
+      let info = {
+        company: this.company,
+        registration: this.registration,
+        name: this.name,
+        sector: this.sector
+      }
+      Vue.ls.set('Buddy', info, 60 * 60 * 100000);
+      this.firstLogin = false;
+    },
+    send: function(value) {
+      console.log(value)
+      this.vote = true;
+    }
   }
 });
