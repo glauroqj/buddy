2f836cd516dc5b439da044aec2a871a41e231838
glauroqj <glauro.juliani@hotmail.com>
2017-10-02 22:44:53 -0300

    add: moment.js

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,59 +1,60 @@
-/* verify vote */
-setInterval(function() {
-	Cookies.remove('Buddy-Vote');
-	let login = '';
-	let vote = '';
-	login = Cookies.get('Buddy-Login');
-	vote = Cookies.get('Buddy-Vote');
+(function () {
+	/* verify vote */
+	setInterval(function() {
+		Cookies.remove('Buddy-Vote');
+		let login = '';
+		let vote = '';
+		login = Cookies.get('Buddy-Login');
+		vote = Cookies.get('Buddy-Vote');
 
-	if ( login === '' || login === null || login === undefined ) {
+		if ( login === '' || login === null || login === undefined ) {
 
-		if (!('Notification' in window)) {
-			alert('This browser does not support desktop notification');
+			if (!('Notification' in window)) {
+				alert('This browser does not support desktop notification');
+			}
+			else if (Notification.permission === 'granted') {
+				var notification = new Notification('Olá, faça login por favor!');
+				voteAgain();
+			}
+			else if (Notification.permission !== 'denied') {
+				Notification.requestPermission(function (permission) {
+					if (permission === 'granted') {
+						var notification = new Notification('Olá, faça login por favor!');
+						voteAgain();
+					}
+				});
+			}
+
+		} else if ( vote === '' || vote === null || vote === undefined ) {
+
+			if (!('Notification' in window)) {
+				alert('This browser does not support desktop notification');
+			}
+			else if (Notification.permission === 'granted') {
+				var notification = new Notification('Olá, como está seu dia hoje?');
+				voteAgain();
+			}
+			else if (Notification.permission !== 'denied') {
+				Notification.requestPermission(function (permission) {
+					if (permission === 'granted') {
+						var notification = new Notification('Olá, como está seu dia hoje?');
+						voteAgain();
+					}
+				});
+			}
+
+		} else {
+
 		}
-		else if (Notification.permission === 'granted') {
-			var notification = new Notification('Olá, faça login por favor!');
-			voteAgain();
-		}
-		else if (Notification.permission !== 'denied') {
-			Notification.requestPermission(function (permission) {
-				if (permission === 'granted') {
-					var notification = new Notification('Olá, faça login por favor!');
-					voteAgain();
-				}
-			});
-		}
 
-	} else if ( vote === '' || vote === null || vote === undefined ) {
+	}, 86400000); /* 3600000 1hour */
 
-		if (!('Notification' in window)) {
-			alert('This browser does not support desktop notification');
-		}
-		else if (Notification.permission === 'granted') {
-			var notification = new Notification('Olá, como está seu dia hoje?');
-			voteAgain();
-		}
-		else if (Notification.permission !== 'denied') {
-			Notification.requestPermission(function (permission) {
-				if (permission === 'granted') {
-					var notification = new Notification('Olá, como está seu dia hoje?');
-					voteAgain();
-				}
-			});
-		}
+	function voteAgain() {
 
-	} else {
-
-	}
-
-}, 86400000); /* 3600000 1hour */
-
-function voteAgain() {
-
-	chrome.tabs.create({
-		url: chrome.extension.getURL('pages/popup.html'),
-		active: true
-	}, function(tab) {
+		chrome.tabs.create({
+			url: chrome.extension.getURL('pages/popup.html'),
+			active: true
+		}, function(tab) {
             // chrome.windows.create({
             //     tabId: tab.id,
             //     type: 'popup',
@@ -62,4 +63,6 @@ function voteAgain() {
             // });
         });
 
-}
+	}
+/*end js*/
+})();
